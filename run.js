850849195dc54e998ee201c17519c375879bbf70
Tyler Bowler <tbowler@omniboardinc.com>
2017-02-01 09:29:37 -0600

    dotenv is now a dev dependency

--- run.js
+++ run.js
@@ -1,3 +1,9 @@
-require('dotenv').config()
+try {
+  require('dotenv').config();
+  console.log('dotenv loaded');
+} catch(e) {
+  console.debug("dotenv is not loaded");
+}
+
 var Backup = require('./firebaseBackup');
 var b = new Backup();
