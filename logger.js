e7190751ea9ed3b213afbdfeddfc914e0ec58b78
Tyler Bowler <tbowler@omniboardinc.com>
2017-02-01 12:54:22 -0600

    more code cleanup

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
-var simpleNodeLogger = require('simple-node-logger');
+var simpleNodeLogger = require("simple-node-logger");
 
 module.exports = simpleNodeLogger.createSimpleLogger({
-  timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
-  level: 'all',
+  timestampFormat: "YYYY-MM-DD HH:mm:ss.SSS",
+  level: "all",
   sync: true,
 });
