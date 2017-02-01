4357e442ed1422d01a61cbee78625b5c97d66c01
Tyler Bowler <tbowler@omniboardinc.com>
2017-02-01 09:33:26 -0600

    debug is not a console method

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ try {
   require('dotenv').config();
   console.log('dotenv loaded');
 } catch(e) {
-  console.debug("dotenv is not loaded");
+  console.log("dotenv is not loaded");
 }
 
 var Backup = require('./firebaseBackup');
